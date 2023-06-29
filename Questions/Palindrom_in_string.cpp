79abdfbae56c5dcfa5ee7e87f0943499da0c79dc
Ashvin Bari <ashvinbari6@gmail.com>
2023-06-29 23:59:00 +0530

    commit
    29-06-2023

---
+++ Questions/Palindrom_in_string.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int palindrom(string s)
+{
+    int i=0;
+    int n=s.length();
+    int j=n-1;
+
+    while(i<=j)
+    {
+        if(s[i]!=s[j])
+        {
+            return 0;
+        }
+        else
+        {
+            i++;
+            j--;
+        }
+    }
+    return 1;
+}
+
+int main(){
+    string s="abba";
+    int x=palindrom(s);
+    if(x)
+    {
+        cout<<"Palindrom.";
+    }
+    else
+    {
+        cout<<"Not Palindrom.";
+    }
+return 0;
+}
