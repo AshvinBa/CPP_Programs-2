d21f9cc8273149e56a67e2b26e1b51fc974a5928
Ashvin Bari <ashvinbari6@gmail.com>
2023-09-27 18:58:14 +0530

    commited
    27-09-2023

--- Questions/new.cpp
+++ Questions/new.cpp
@@ -1,6 +1,34 @@
+//Three way Partision. 
+#include<bits/stdc++.h>
 #include<iostream>
 using namespace std;
+void threeWayPartition(vector<int>& array,int a, int b)
+    {
+        int l=0;
+        int r=array.size()-1;
+        
+        for(int i=0;i<=r;i++)
+        {
+            if(array[i]<a)
+            {
+                swap(array[i],array[l]);
+                l++;
+            }
+            else if(array[i]>b)
+            {
+                swap(array[i],array[r]);
+                r--;
+                i--;
+            }
+        }
+    }
 int main(){
-    cout<<"hi";
+    int a=1,b=3;
+    vector<int>arr={1,2,3};
+    cout<<"Answer: ";
+    for(int i=0;i<arr.size();i++)
+    {
+        cout<<" "<<arr[i];
+    }
 return 0;
 }
