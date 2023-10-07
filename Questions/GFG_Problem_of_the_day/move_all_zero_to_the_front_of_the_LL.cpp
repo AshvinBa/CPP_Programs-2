2e37e92d917f8a02d67af1d19fbe3766c856e4b0
Ashvin Bari <ashvinbari6@gmail.com>
2023-10-08 00:10:37 +0530

    commited
    08-10-2023

--- Questions/GFG_Problem_of_the_day/move_all_zero_to_the_front_of_the_LL.cpp
+++ Questions/GFG_Problem_of_the_day/move_all_zero_to_the_front_of_the_LL.cpp
@@ -1,6 +1,107 @@
-#include<iostream>
+/*
+Move all zeros to the front of the linked list
+
+Given a linked list, the task is to move all 0s to the front of the linked list. The order of all another element except 0 should be same after rearrangement.
+
+Example 1:
+Input:  0 -> 4 -> 0 -> 5 -> 0
+Output: 0 -> 0 -> 0 -> 4 -> 5
+Explanation: After moving all 0s of the given
+list to the front, the list is:
+0 -> 0 -> 0 -> 4 -> 5
+
+Example 2:
+Input: 0 -> 1 -> 0 -> 1 -> 2
+Output: 0 -> 0 -> 1 -> 1 -> 2
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function moveZeroes() which takes head node of the linked list as input parameter.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+Constraints:
+1 <= N <= 50
+*/
+
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-    cout<<"f";
-return 0;
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void push(struct Node **head_ref, int new_data) {
+    struct Node *new_node = new Node(new_data);
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void moveZeroes(struct Node **head);
+
+void display(struct Node *head) {
+    while (head != NULL) {
+	    cout << head->data << " ";
+	    head = head->next;
+    }
+    cout <<endl;
+}
+
+int main()
+{
+    int t;
+    cout<<"Enter the Number of test cases: ";
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cout<<"Enter the Limit: ";
+        cin>>n;
+        int q;
+        vector<int> v;
+        struct Node *Head=NULL;
+        cout<<"Enter the values: ";
+        for(int i=0;i<n;i++)
+        {
+            cin>>q;
+            v.push_back(q);
+        }
+        reverse(v.begin(),v.end());
+        for(auto q:v)
+            push(&Head,q);
+        moveZeroes(&Head);
+        display(Head);
+
+    }
+}
+
+void moveZeroes(struct Node **head)
+{
+    if(*head == NULL)
+    return;
+    
+    Node *temp=(*head)->next,*prev=*head;
+    
+    while(temp!=NULL)
+    {
+        if(temp->data==0)
+        {
+            Node *curr=temp;
+            temp=temp->next;
+            prev->next=temp;
+            curr->next=*head;
+            *head=curr;
+        }
+        else
+        {
+            prev=temp;
+            temp=temp->next;
+        }
+    }
+    
 }
--- Questions/pairwise_swap_element_of_a_LL.cpp
+++ Questions/pairwise_swap_element_of_a_LL.cpp
@@ -1,6 +1,122 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"f";
-return 0;
+/*
+Pairwise swap elements of a linked list
+Given a singly linked list of size N. The task is to swap elements in the linked list pairwise.
+For example, if the input list is 1 2 3 4, the resulting list after swaps will be 2 1 4 3.
+Note: You need to swap the nodes, not only the data. If only data is swapped then driver will print -1.
+
+Example 1:
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+Output: 
+2 1 4 2 6 5 8 7
+Explanation: 
+After swapping each pair considering (1,2), (2, 4), (5, 6).. so on as pairs, we get 2, 1, 4, 2, 6, 5, 8, 7 as a new linked list.
+
+Example 2:
+Input:
+LinkedList: 1->3->4->7->9->10->1
+Output: 
+3 1 7 4 10 9 1
+Explanation: 
+After swapping each pair considering (1,3), (4, 7), (9, 10).. so on as pairs, we get 3, 1, 7, 4, 10, 9, 1 as a new linked list.
+Your Task:
+The task is to complete the function pairWiseSwap() which takes the head node as the only argument and returns the head of modified linked list.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ N ≤ 105
+
+*/
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+class Solution
+{
+    public:
+    Node* pairWiseSwap(struct Node* head) 
+    {
+        Node* temp=new Node(0);
+        temp->next=head;
+        Node* prev=temp;
+        Node* curr=head;
+        
+        while(curr!=NULL and curr->next!=NULL)
+        {
+            Node* nextNode=curr->next;
+            Node* nodeToNextNode=curr->next->next;
+            nextNode->next=curr;
+            curr->next=nodeToNextNode;
+            prev->next=nextNode;
+            prev=curr;
+            curr=nodeToNextNode;
+        }
+        return temp->next;
+    }
+};
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+int main() 
+{ 
+	int t;
+    cout<<"Enter the no of test cases: ";
+	cin>>t;
+	while(t--)
+	{
+		int n;
+        cout<<"Enter the limit: ";
+		cin>>n;
+
+		int data;
+        cout<<"Enter the values: ";
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		map<Node*, int> mp;
+		mp[head] = head->data;
+		for (int i = 0; i<n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+			mp[tail] = tail->data;
+		}
+		struct Node *failure = new Node(-1);
+		Solution ob;
+		head = ob.pairWiseSwap(head);
+		int flag = 0;
+		struct Node *temp = head;
+		while(temp){
+		    if(mp[temp] != temp->data){
+		        flag = 1;
+		        break;
+		    }
+		    temp = temp->next;
+		}
+		if(flag)
+		    printList(failure);
+		else
+		    printList(head);
+	}
+	return 0; 
 }
