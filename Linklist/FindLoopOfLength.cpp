56fe39f82a81df2faf3d1df48fe8c99d45bf5049
Ashvin Bari <ashvinbari6@gmail.com>
2024-09-06 12:37:22 +0530

    Create FindLoopOfLength
    commited

---
+++ Linklist/FindLoopOfLength.cpp
@@ -0,0 +1,121 @@
+/*
+Find length of Loop
+Given the head of a linked list, determine whether the list contains a loop. If a loop is present, return the number of nodes in the loop, otherwise return 0.
+
+Note: 'c' is the position of the node which is the next pointer of the last node of the linkedlist. If c is 0, then there is no loop.
+
+Examples:
+Input: LinkedList: 25->14->19->33->10->21->39->90->58->45, c = 4
+Output: 7
+Explanation: The loop is from 33 to 45. So length of loop is 33->10->21->39-> 90->58->45 = 7. 
+The number 33 is connected to the last node of the linkedlist to form the loop because according to the input the 4th node from the beginning(1 based indexing) 
+will be connected to the last node for the loop.
+ 
+Input: LinkedList: 5->4, c = 0
+Output: 0
+Explanation: There is no loop.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= no. of nodes <= 106
+0 <= node.data <=106
+0 <= c<= n-1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void loopHere(Node *head, Node *tail, int position) {
+    if (position == 0)
+        return;
+
+    Node *walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+    
+    int loopCount(Node* rabbit,Node* turtle)
+    {
+        int count=1;
+        rabbit=rabbit->next;
+        
+        while(rabbit!=turtle)
+        {
+            count++;
+            rabbit=rabbit->next;
+        }
+        return count;
+    }
+    
+    int countNodesinLoop(Node *head) {
+        
+        Node* turtle=head;
+        Node* rabbit=head;
+        
+        while(rabbit!=NULL && rabbit->next!=NULL)
+        {
+            rabbit=rabbit->next->next;
+            turtle=turtle->next;
+            
+            if(rabbit==turtle)
+            {
+                return loopCount(rabbit,turtle);
+            }
+        }
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        loopHere(head, tail, k);
+
+        Solution ob;
+        cout << ob.countNodesinLoop(head) << endl;
+    }
+    return 0;
+}
